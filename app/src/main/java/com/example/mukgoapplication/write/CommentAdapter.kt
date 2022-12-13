a30b07d189541dffdc923bfa25f0a0a108a58223
Brinicle177 <kimkr0421@naver.com>
2022-12-13 19:10:32 +0900

    comment관련 class 생성

---
+++ app/src/main/java/com/example/mukgoapplication/write/CommentAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.mukgoapplication.write
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mukgoapplication.R
+
+class CommentAdapter(val context: Context, val cmtList: ArrayList<CommentVO>): RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.comment_temp, null)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return cmtList.size
+    }
+}
